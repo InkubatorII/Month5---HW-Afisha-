6e8c0cd885d2d5609129d03ac2a55e6ffcb1e804
InkubatorII <holloward@gmail.com>
2024-11-04 23:53:39 +0600

    hw1.1

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -16,7 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from movie_app.views import (
+    DirectorListView, DirectorDetailView,
+    MovieListView, MovieDetailView,
+    ReviewListView, ReviewDetailView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('directors/', DirectorListView.as_view(), name='director-list'),
+    path('directors/<int:pk>/', DirectorDetailView.as_view(), name='director-detail'),
+    path('movies/', MovieListView.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
+    path('reviews/', ReviewListView.as_view(), name='review-list'),
+    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
 ]
--- movie_app/views.py
+++ movie_app/views.py
@@ -1,5 +1,30 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
+from movie_app.models import Director, Movie, Review
+from movie_app.serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
 
-@api_view([GET])
+class DirectorListView(generics.ListCreateAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+class DirectorDetailView(generics.RetrieveAPIView):
+    queryset = Director.objects.all()
+    serializer_class = DirectorSerializer
+
+class MovieListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieDetailView(generics.RetrieveAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class ReviewListView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetailView(generics.RetrieveAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
