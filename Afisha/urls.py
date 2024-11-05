27ba5b621aae28e941a63b4a23f064923ffd426f
InkubatorII <holloward@gmail.com>
2024-11-05 13:30:23 +0600

    hw1.2

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -25,10 +25,10 @@ from movie_app.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('directors/', DirectorListView.as_view(), name='director-list'),
-    path('directors/<int:pk>/', DirectorDetailView.as_view(), name='director-detail'),
-    path('movies/', MovieListView.as_view(), name='movie-list'),
-    path('movies/<int:pk>/', MovieDetailView.as_view(), name='movie-detail'),
-    path('reviews/', ReviewListView.as_view(), name='review-list'),
-    path('reviews/<int:pk>/', ReviewDetailView.as_view(), name='review-detail'),
+    path('/api/v1/directors/', DirectorListView.as_view(), name='director-list'),
+    path('/api/v1/directors/<int:id>/', DirectorDetailView.as_view(), name='director-detail'),
+    path('/api/v1/movies/', MovieListView.as_view(), name='movie-list'),
+    path('/api/v1/movies/<int:id>/', MovieDetailView.as_view(), name='movie-detail'),
+    path('/api/v1/reviews/', ReviewListView.as_view(), name='review-list'),
+    path('/api/v1/reviews/<int:id>/', ReviewDetailView.as_view(), name='review-detail'),
 ]
